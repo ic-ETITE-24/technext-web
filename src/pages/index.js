5ee41787e0369d98e5d50718c73739d7d7496acf
NishantGupta786 <nishantgupta2325@gmail.com>
2023-08-18 00:43:54 +0530

    feat: Add the Regulations and registrations components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,13 @@
 import Main from "@/components/Main";
+import Registration from "@/components/registration";
+import Regulations from "@/components/regulations";
 
 export default function Home() {
   return (
     <div>
       <Main />
+      <Regulations />
+      <Registration />
     </div>
   );
 }
