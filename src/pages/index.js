0fe2d740a7b688c33e3094149f1f39b801b2dbf4
Pratham Gupta <prathamgupta.pg1@gmail.com>
2023-08-19 01:18:09 +0530

    feat: added footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,16 @@ import Main from "@/components/Main";
 import Registration from "@/components/registration";
 import Regulations from "@/components/regulations";
 import AboutEvent from "@/components/AboutEvent";
+import Footer from "@/components/Footer";
 
 export default function Home() {
-  return (
-    <div>
-      <Main />
-      <AboutEvent />
-      <Regulations />
-      <Registration />
-    </div>
-  );
+    return (
+        <div>
+            <Main />
+            <AboutEvent />
+            <Regulations />
+            <Registration />
+            <Footer />
+        </div>
+    );
 }
