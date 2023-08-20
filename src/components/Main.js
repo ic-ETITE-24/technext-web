c57717bc4ba3ecf43eb28daaf2d6e25afa64ac7c
Gaurav Choudhary <gloonaich@gmail.com>
2023-08-20 19:25:43 +0530

    Changed the downlaod button in mobile view

--- src/components/Main.js
+++ src/components/Main.js
@@ -9,7 +9,7 @@ const Main = () => {
     <>
       <div className={CSS.background}>
         <div className="flex justify-between items-center w-full pt-6">
-          <div className="flex justify-between items-center felx mx-4 ml-5 sm:ml-20 sm:max-w-sm">
+          <div className="flex justify-between items-center felx mx-4 ml-5 sm:ml-20 sm:max-w-md w-[300px]">
             <Image
               className="felx sm:w-[52px] w-[38px]"
               src={logo}
@@ -22,18 +22,22 @@ const Main = () => {
             </span>
           </div>
           <div className="text-center flex flex-wrap justify-end items-center mr-4">
-            <div className="flex mx-[30px]">
+            <div className="flex mx-[10px]">
               <a
                 href="/input_page"
                 className="uppercase p-2 sm:text-xl text-[10px] hover:text-gray-300 active:text-white"
               >
                 Register Now
               </a>
-              <Image src={register} alt="register" />
+              <Image
+                className="sm:w-[20px] w-[15px]"
+                src={register}
+                alt="register"
+              />
             </div>
             <a
               href="/input_page"
-              className="uppercase mx-[15px] my-1 sm:mx-[30px] px-2 sm:px-5 py-1 sm:py-2 sm:text-xl text-[10px] bg-[#FF7A00] sm:rounded-lg rounded-md hover:bg-[#ff9837] hover:text-white hover:font-[400] active:bg-[#FF7A00]"
+              className="uppercase mx-[15px] my-1 sm:mx-[30px] px-2 sm:px-5 py-1 sm:py-2 sm:text-xl text-[10px] bg-[#FF7A00] sm:rounded-lg rounded-md hover:bg-[#ff9837] hover:text-white hover:font-[400] active:bg-[#FF7A00] xl:block hidden"
             >
               Download Brochure
             </a>
@@ -46,6 +50,14 @@ const Main = () => {
             alt="title"
             quality={100}
           />
+        </div>
+        <div className="flex justify-center xl:hidden mb-4">
+          <a
+            href="/input_page"
+            className="uppercase px-4 py-1.5 sm:py-2 sm:text-lg text-[10px] bg-[#FF7A00] sm:rounded-lg rounded-md hover:bg-[#ff9837] hover:text-white hover:font-[400] active:bg-[#FF7A00]"
+          >
+            Download Brochure
+          </a>
         </div>
       </div>
     </>
