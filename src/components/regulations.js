72124881815c28503cd2c77e62212854cdc82d2f
NishantGupta786 <nishantgupta2325@gmail.com>
2023-08-21 02:57:25 +0530

    fix:Change the alignment of the bottom part of regulations and registration component

--- src/components/regulations.js
+++ src/components/regulations.js
@@ -9,7 +9,7 @@ function Regulations() {
         <div className="mx-10 md:mx-20 text-lg sm:text-4xl font-[600] py-10">
           Regulations
         </div>
-        <div className="flex lg:flex-nowrap flex-wrap justify-around items-center">
+        <div className="flex lg:flex-nowrap flex-wrap justify-around items-center lg:h-[600px]">
           <div className="flex items-center four gap-6 sm:pl-4">
             <div className="text-orange-500 text-6xl sm:pl-12 2xl:pl-20 sm:text-9xl md:text-[160px] 2xl:text-[210px]">
               4
@@ -21,7 +21,7 @@ function Regulations() {
             </div>
           </div>
 
-          <div className="sm:pt-4 pt-6 sm:pl-24 w-[400px] md:w-[800px] md:pl-6 sm:leading-relaxed">
+          <div className="sm:pt-4 pt-6 sm:pl-24 w-[500px] md:w-[700px] md:pl-6 sm:leading-relaxed">
             <ul className="list-disc text-white sm:text-xl md:text-xl md:w-full 2xl:text-2xl 2xl:pt-16 px-14 lg:text-left">
               <li>
                 Participants are required to pay the required registration fee
@@ -48,8 +48,8 @@ function Regulations() {
             </ul>
           </div>
         </div>
-        <div className="flex flex-col md:flex-row pl-10 sm:pl-8 sm:pt-56 pt-10 justify-center md:pt-20 2xl:pt-20 2xl:w-full">
-          <div className="text-white pt-9 px-10 md:pl-12 md:text-lg text-xs opacity-30 py-4 sm:py-0 2xl:w-[800px]">
+        <div className="flex flex-col md:flex-row pl-10 sm:pl-8 sm:pt-56 pt-10 justify-center md:pt-6 2xl:pt-20 lg:w-full">
+          <div className="text-white pt-9 px-10 md:pl-12 md:text-lg text-xs opacity-30 py-4 sm:py-0 lg:w-3/4">
             * 3 MEMBERS CAN ALSO FORM A TEAM
           </div>
           <div className="w-full flex justify-end px-24 pb-10">
