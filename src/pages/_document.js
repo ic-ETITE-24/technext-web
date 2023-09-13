7cfcbdee9c95c3ccc715db9bbf82de602a6c0be0
souvik150 <souvikmukherjee150@gmail.com>
2023-09-13 20:44:00 +0530

    feat: adds

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,29 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Other head elements */}
+        {/* Google Analytics tracking code */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-6EV94V5J2K"
+        ></script>
+        <script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-6EV94V5J2K');
+          `}
+        </script>
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
