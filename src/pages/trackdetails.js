e85433ca4214c0db66f1c9246f7b94abebf07cc4
NishantGupt786 <nishantgupta2325@gmail.com>
2023-10-10 00:16:59 +0530

    fix:tracks page

--- src/pages/trackdetails.js
+++ src/pages/trackdetails.js
@@ -1,10 +1,4 @@
-import Tracks1 from "@/components/tracks1";
-import Tracks2 from "@/components/tracks2";
-import Tracks3 from "@/components/tracks3";
-import Tracks4 from "@/components/tracks4";
-import Tracks5 from "@/components/tracks5";
-import Tracks6 from "@/components/tracks6";
-import Tracks7 from "@/components/tracks7";
+import Trackssingle from "@/components/trackssingle";
 import Head from "next/head";
 import Footer from "@/components/Footer";
 
@@ -15,13 +9,7 @@ function Tracks() {
         <title>Technext | Tracks</title>
         <link rel="icon" type="image/x-icon" href="/favicon.ico"></link>
       </Head>
-      <Tracks1 />
-      <Tracks2 />
-      <Tracks3 />
-      <Tracks4 />
-      <Tracks5 />
-      <Tracks6 />
-      <Tracks7 />
+      <Trackssingle />
       <Footer />
     </>
   );
