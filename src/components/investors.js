bc0b15a02296d57aaa0e032507d32b28dccbf84d
NishantGupt786 <nishantgupta2325@gmail.com>
2023-11-24 18:51:45 +0530

    Add investor

--- src/components/investors.js
+++ src/components/investors.js
@@ -3,6 +3,8 @@ import CSS from "./investors.module.css";
 import expo from "../assets/expo-mark.png";
 import inv1 from "../assets/inv1new.png";
 import inv2 from "../assets/inv2new.png";
+import inv3 from "../assets/investor3.png";
+import inv3title from '../assets/java-capital.svg';
 import { TbWorld } from "react-icons/tb";
 import { FaLinkedin } from "react-icons/fa";
 import Link from "next/link";
@@ -14,7 +16,7 @@ function Investors() {
         <div className="mx-10 text-white md:mx-20 text-lg sm:text-4xl font-[600] py-2 pb-6">
           OUR INVESTMENT PARTNERS
         </div>
-        <div className="flex flex-col justify-center lg:flex-row gap-y-4 lg:mt-20">
+        <div className="flex flex-col justify-center lg:flex-row lg:flex-wrap gap-y-4 lg:mt-20">
           <div className="mx-auto my-auto h-[40%] w-[80%] lg:w-[35%] sm:w-[60%] rounded-md pb-3 backdrop-blur-md backdrop-brightness-125">
             <div className="flex flex-col justify-center">
               <Image
@@ -61,8 +63,31 @@ function Investors() {
               </div>
             </div>
           </div>
+          <div className="mx-auto my-auto h-[40%] w-[80%] lg:w-[35%] sm:w-[60%] rounded-md pb-3 backdrop-blur-md backdrop-brightness-125 lg:mt-10">
+            <div className="flex flex-col justify-center">
+              <Image
+                src={inv3}
+                alt="investor3"
+                quality={100}
+                height={300}
+                width={120}
+                className="self-center py-16 mt-4"
+              />
+              <h1 className="self-center text-center text-white/50 lg:text-2xl">
+              JAVA CAPITAL
+              </h1>
+              <div className="self-center flex justify-center gap-x-4 py-4 text-white">
+                <Link href="https://www.linkedin.com/company/javacapital/">
+                  <FaLinkedin className="stroke-white/25" size={25} />
+                </Link>
+                <Link href="https://www.javacapital.co/">
+                  <TbWorld className="stroke-white/25" size={25} />
+                </Link>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="w-full flex md:justify-end justify-center px-24 pb-10 mt-14 lg:mt-32">
+        <div className="w-full flex md:justify-end justify-center px-24 pb-10 mt-14 lg:mt-24">
           <Image src={expo} alt="expo-mark" quality={100} />
         </div>
       </div>
